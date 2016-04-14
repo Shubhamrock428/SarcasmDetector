d9a3c0c68663dbf72a82d25e0b9e8fbcd612b9d0
Rajvi Mehta <rajvimam@usc.edu>
2016-04-14 00:49:38 -0700

    #10

---
+++ classifier/svm/SentimentFeatureWithPosition.py
@@ -0,0 +1,35 @@
+import json, io, itertools
+def flattenListOfLists(lst):
+    result = []
+    for sublist in lst:
+        result.extend(sublist)
+    return result
+def chunkify(lst,n):
+    return [ lst[i::n] for i in xrange(n) ]
+
+def fillSameValue(lst,val):
+    lst = [1 for i in range(0,len(lst))]
+
+#sentiment Tagged File
+with open("sentiment_model.txt","r") as f:
+    classifier = json.load(f)
+
+#Input File
+with open("processedtweet.txt","r") as f:
+    content = f.readlines()
+
+vector={}
+for line in content:
+    words = line.split()
+    list = chunkify(range(100),len(words))
+    for i in range(0,len(words)):
+        if words[i] in classifier:
+            value = int(classifier[words[i]])
+        else :
+            value = int(0)
+        fillSameValue(list[i],value)
+    vector.add(flattenListOfLists(list))
+
+
+
+
