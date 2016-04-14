9b1c465b3f57709dfa98c041755934a89498b70e
smadha <msharan@usc.edu>
2016-04-14 00:43:50 -0700

    sum classifier for textual features

--- classifier/svm/sarcasm-classify.py
+++ classifier/svm/sarcasm-classify.py
@@ -1,25 +1,41 @@
 from sklearn import svm
 from sklearn.feature_extraction import *
 from collections import Counter
-word_counts = []
 
-with open("ProcessedtextTweets.txt", "r") as o:
+# Array of dicts. dicts have count of words
+word_counts = []
+# 1 = sarcastic , 0 = non-sarcastic
+observations = []
+
+# Read sarcastic 
+with open("sarcastic.txt", "r") as o:
     line = o.readline()
     
     while (line):
-        line.strip()
-        word_count = Counter()
-        words = line.split(" ")
-        for word in words:
-            print word
-            word_count.update(word)
-            
-        print word_count
+        line = line.strip()
+        word_count = Counter(filter(None,line.split(" ")))
+        word_counts.append(dict(word_count) )
+        # 1 for sarcastic
+        observations.append(1)
         line = o.readline()
     
     o.close()
 
-word_counts = [{'word1': 1,'word2': 1,},{'word1': 2,'word2': 2,},{'word3': 2,'word4': 2,},{'word3': 3,'word4': 3,}, ]
+# Read non sarcastic
+with open("non_sarcastic.txt", "r") as o:
+    line = o.readline()
+    
+    while (line):
+        line = line.strip()
+        word_count = Counter(filter(None,line.split(" ")))
+        word_counts.append(dict(word_count) )
+        # 0 for non sarcastic
+        observations.append(0)
+        line = o.readline()
+    
+    o.close()
+
+# word_counts = [{'word1': 1,'word2': 1,},{'word1': 2,'word2': 2,},{'word3': 2,'word4': 2,},{'word3': 3,'word4': 3,} ]
 
 vectorize = DictVectorizer()
 
@@ -28,12 +44,10 @@ feature_vectorized = vectorize.fit_transform(word_counts)
 # # vector array  
 features_arr = feature_vectorized.toarray()
 
-# # 1 = sarcastic , 0 = non-sarcastic
-observations = [1, 1, 0, 0]
-
 clf = svm.SVC()
+# clf = SGDClassifier(loss="hinge", penalty="l2")
 
 clf.fit(features_arr, observations)  
-
-print clf.predict([[ 0, 1, 2, 2]])
+print vectorize.get_feature_names()
+# print clf.predict([[ 0, 1, 2, 2]])
 
