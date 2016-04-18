a609985a28adc77b4c070ac602f4cea73c260adc
Rajvi Mehta <rajvimam@usc.edu>
2016-04-17 21:15:44 -0700

    Update InputPreprocessTweet.java

--- process-tweet/src/main/java/InputPreprocessTweet.java
+++ process-tweet/src/main/java/InputPreprocessTweet.java
@@ -18,51 +18,8 @@ import edu.usc.nlp.mnlp.hindi.HindiUtil;
 		
 
 	public static void main(String[] args) {
+		PreprocessTweet pt = new PreprocessTweet();
 		String tweet = args[0];
-		System.out.println(process(tweet));
+		System.out.println(pt.process(tweet));
 	}
-	
-	private static String process(String tweet) {
-		// TODO Auto-generated method stub
-		tweet = tweet.trim();
-
-		tweet = tweet.replaceAll("S?@\\S+\\s?", "");
-		tweet = tweet.replaceAll("www?.\\S+\\s?", "");
-		tweet = tweet.replaceAll("https?(://\\S+\\s?)?", "");
-		/*
-		 * this should be tweet.replaceAll("\\s*\\b#sarcasm\\b\\s*",""); tweet.replaceAll("\\s*\\b#sarcastic\\b\\s*","");
-		 */
-
-		// tweet=tweet.replaceAll("#?sarcasm|#?sarcastic", "");
-	
-		tweet = tweet.replaceAll("#[A-Za-z]+", "");
-		tweet = tweet.replaceAll("#[^ ]+", "");
-		tweet = tweet.replaceAll("[0-9]", "");
-		tweet = tweet.replaceAll("\\(|\\)|\\[|\\]", "");
-		tweet = tweet.replaceAll("\\/", "");
-		// tweet=tweet.replaceAll("\\...", "");
-		tweet = tweet.replaceAll("_", "");
-		tweet = tweet.replaceAll("\\*", "");
-		tweet = tweet.replaceAll("\\!|\\?|\\;|\\,|\\:|\\.", "");
-		tweet = tweet.replaceAll("|", "");
-		// tweet=tweet.replaceAll("[^\\x00-\\x7f-\\x80-\\xad]", "");
-		tweet = tweet.replaceAll("[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]", "");
-		tweet = tweet.replaceAll("[a-zA-Z]+", "");
-		tweet = tweet.replaceAll("\\\\", "");
-		tweet = tweet.replaceAll("\"", "");
-		//System.out.println(tweet);
-		StringBuffer sb = new StringBuffer();
-		for (String word : tweet.split(" ") )
-		{
-			sb.append(HindiUtil.getStemmedHindiWord(word));
-			sb.append(" ");
-		}
-		tweet = sb.toString();
-		//System.out.println(tweet);
-		
-		return tweet;
-	}
-
-	
-	
 }
