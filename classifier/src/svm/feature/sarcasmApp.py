bd94642d7415035d67c96d459aabeac4995aaaa5
smadha <msharan@usc.edu>
2016-04-17 22:30:41 -0700

    stemmed in app

--- classifier/src/svm/feature/sarcasmApp.py
+++ classifier/src/svm/feature/sarcasmApp.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import os,string,json,math,sys
 from nbclassify import NBSentence
 from sentiment_feature_position import getSVMVectorSentiPosAPP
@@ -5,11 +7,22 @@ from sentiment_feature_with_count import getSVMVectorSentiCntAPP
 from word_feature import getSVMVectorBagWordsAPP 
 
 
+JAVA_CLI = "java -cp ../../../../process-tweet/target/process-tweet-0.0.1-SNAPSHOT-jar-with-dependencies.jar InputPreprocessTweet '{0}' "
 
-NBSentence(sys.argv[1])
+input_line = sys.argv[1]
 
-# getSVMVectorBagWordsAPP(sys.argv[1])
-getSVMVectorSentiCntAPP(sys.argv[1])
-getSVMVectorSentiPosAPP(sys.argv[1])
+print "Input provided - ", input_line
+
+stemmed_op = os.popen(JAVA_CLI.format(input_line)).read()
+if(stemmed_op):
+    input_line = stemmed_op
+    
+print "Stemmed it to - ", stemmed_op
+
+NBSentence(input_line)
+
+# getSVMVectorBagWordsAPP(input_line)
+getSVMVectorSentiCntAPP(input_line)
+getSVMVectorSentiPosAPP(input_line)
 
 
