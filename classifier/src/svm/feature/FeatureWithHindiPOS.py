8587b31ecd02e5841a7f368fc02400b76b78d8f0
Rajvi Mehta <rajvimam@usc.edu>
2016-04-17 21:34:30 -0700

    Updated with svm Stuff

--- classifier/src/svm/feature/FeatureWithHindiPOS.py
+++ classifier/src/svm/feature/FeatureWithHindiPOS.py
@@ -1,17 +1,16 @@
-tagged_file = "hindioutput.txt"
+from sklearn import svm
+from sklearn.externals import joblib
+from sklearn.feature_extraction import *
 
-with open(tagged_file,"r") as f:
-    content = f.readlines()
-
-
-def getSVMVector():
+def getSVMVector(tagged_file):
+    with open(tagged_file,"r") as f:
+        content = f.readlines()
     count = 0
     tags = ['NN','PSP','VM','SYM','JJ','VAUX','NNP','XC','PRP','CC','RP','QC','NST','DEM','QF','NEG','RB','INTF','RDP','QO','WQ','INJ']
     vector=[]
     v = [0]*len(tags)
     print v
     for line in content:
-
         words = line.split()
         if len(words) < 2:
             if words[0]=='</s>':
@@ -34,3 +33,48 @@ def getSVMVector():
     return vector
 print getSVMVector()
 
+def main_fn() :
+    sarcastic_corpus="../../../../process-tweet/sarcastic_proc.txt"
+    non_sarcastic_corpus="../../../../process-tweet/nonsarcastic_proc.txt"
+    test_corpus_sarcastic="../test.txt"
+    test_corpus_nonsarcastic="../test.txt"
+
+    sarcastic_corpus_output=""
+    non_sarcastic_corpus_output=""
+    test_corpus_sarcastic_output=""
+    test_corpus_nonsarcastic_output=""
+
+    sar_features_arr = getSVMVector(sarcastic_corpus_output)
+    print sar_features_arr
+    observations = [1] * len(sar_features_arr)
+
+    non_features_arr = getSVMVector(non_sarcastic_corpus_output)
+    observations += [0] * len(non_features_arr)
+
+    print len(sar_features_arr) + len(non_features_arr)
+    print len(observations)
+
+    clf = svm.SVC()
+    # clf = SGDClassifier(loss="hinge", penalty="l2")
+
+    clf.fit(sar_features_arr + non_features_arr, observations)
+
+    print joblib.dump(clf, '../predict/senti_feature_pos/senti_feature_pos.pkl')
+
+    print "dumped senti_feature_pos model"
+    # print s
+    # This list of features is needed to create a vector for prediction
+    # print vectorize.get_feature_names()
+
+    # test createPredictionVector
+    test_vector_sarcastic = getSVMVector(test_corpus_sarcastic_output)
+    test_vector_nonsarcastic= getSVMVector(test_corpus_nonsarcastic_output)
+    print clf.predict(test_vector_sarcastic)
+    print clf.predict(test_vector_nonsarcastic)
+
+    #print vectors in files for F1
+
+print __name__
+if __name__ == "__main__":
+    # stuff only to run when not called via 'import' here
+    main_fn()
