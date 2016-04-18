c939dc00d5e4365a2c29b5529ab78e6f584e453c
Rajvi Mehta <rajvimam@usc.edu>
2016-04-17 18:50:36 -0700

    Hindi POS
    Run the POS tagging and give it's output file as input here.

---
+++ classifier/src/svm/feature/FeatureWithHindiPOS.py
@@ -0,0 +1,29 @@
+tagged_file = "hindioutput.txt"
+
+with open(tagged_file,"r") as f:
+    content = f.readlines()
+
+
+def getSVMVector():
+    tags = ['NN','PSP','VM','SYM','JJ','VAUX','NNP','XC','PRP','CC','RP','QC','NST','DEM','QF','NEG','RB','INTF','RDP','QO','WQ','INJ']
+    vector=[]
+    v = [0]*len(tags)
+    print v
+    for line in content:
+
+        words = line.split()
+        if len(words) < 2:
+            if words[0]=='</s>':
+                vector.append(v)
+                v = [0]*len(tags)
+            continue
+        actual_word = words[0]
+        pos_tag = words[2]
+
+        if pos_tag in tags:
+            index = tags.index(pos_tag)
+            v[index]+=1
+    vector.append(v)
+    return vector
+print getSVMVector()
+
